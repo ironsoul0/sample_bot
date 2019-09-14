fbb3da26a04cfb50b5c56506a6b5dc5e665080e4
ironsoul <timka2609@gmail.com>
2019-09-14 17:50:18 +0600

    Some dependencies

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hi there!');
+const axios = require('axios');
+
+axios.get('https://careers.google.com/api/jobs/jobs-v1/search/?category=SOFTWARE_ENGINEERING&degree=BACHELORS&employment_type=INTERN')
+	.then(function (result) {
+		console.log(result.data);
+	});
